48162655820dc5ca6a85210d5ccc6a951f5779c4
ahllyy <amanda.halley@keyin.com>
2025-02-24 19:09:37 -0330

    inserted logic into domain classes aircraft and airport

--- src/main/java/com/keyin/domain/Aircraft.java
+++ src/main/java/com/keyin/domain/Aircraft.java
@@ -1,4 +1,75 @@
 package com.keyin.domain;
 
+import java.util.Objects;
+
 public class Aircraft {
+
+    private long id;
+    private String type;
+    private String airlineName;
+    private int numberOfPassengers;
+
+    //default constructor
+    public Aircraft() {
+    }
+
+    //constructor for type of aircraft
+    public Aircraft(String type) {
+        this.type = type;
+    }
+
+    //constructor initializing all fields of an aircraft
+    public Aircraft(long id, String type, String airlineName, int numberOfPassengers) {
+        this.id = id;
+        this.type = type;
+        this.airlineName = airlineName;
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    //getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    //comparing aircrafts equality from their type
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aircraft aircraft = (Aircraft) o;
+        return Objects.equals(type, aircraft.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
 }
